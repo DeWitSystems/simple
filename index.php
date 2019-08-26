6bc444a3ee06c19ce07ab30a3642d817863a62fc
northphex <54204550+northphex@users.noreply.github.com>
2019-08-26 20:56:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<php bloginfo ('charset'); ?>">
+	<link rel="stylesheet"
+	href="<?php bloginfo ('stylesheet_url'); ?>">
+	<?php wp_head(); ?>
+	<title><?php bloginfo ('name'); ?></title>
+</head>
+<header>
+	<h1><?php bloginfo ('name') ;?> </h1>
+</header>
